8a5ca5df38f5f9eb488ca0194b1891c5d218aa22
kimidimik <kimidimik@gmail.com>
2022-08-28 22:08:27 +0900

    modified:   package-lock.json 	modified:   package.json 	modified:   src/server.js

--- src/server.js
+++ src/server.js
@@ -1,28 +1,52 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const shortId = require('shortid')
 
 const app = express()
 
 app.use(express.static('public'))
+app.use(bodyParser.json()) // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  extended: true
+}))
 
 mongoose.connect('mongodb://localhost:27017/userslinks')
-
   .then(() => {
     console.log('MongoDB connected! Nice, Dima')
   })
 
 app.get('/', (req, res) => {
-  // res.send('MotherFucker! Yeah!')
   res.render('index')
 })
 
 app.post('/', (req, res) => {
-  res.send('OK')
+  // @TODO:
+  // get value of url fied
+  const url = req.body.url
+  // generate short url
+  // @todo: validate URL
+  res.send({
+    url,
+    shortUrl: 'http://localhost:3000/l/' + shortId.generate()
+  })
+
+  // generate unique ID (npm i uuid)
+  // crate a link in the database (uniqueID + url + shortUrl) (mongoose)
+  // redirect to /link/:uniqueID (express redirect)
 })
 
-app.get('/link-management-id', (req, res) => {
+app.get('/link/:uniqueID', (req, res) => {
+  // get uniqueID from the request
+  // read link info from database by uniqueID
+  // if not found render 404
+  // if found render url information
   res.render('link-management-id')
+})
+
+app.get('/l/:shortId', (req, res) => {
+  // redirect to original url if exists
 })
 
 app.use(expressLayouts)
